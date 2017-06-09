9d778f3c1c87ad41c3692834b349d5ecb75a3b26
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-09 09:06:10 -0600

    Initial Project Creation/Cleanup

---
+++ MovieSearch/MovieSearch/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MovieSearch
+//
+//  Created by Bradley GIlmore on 6/9/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
