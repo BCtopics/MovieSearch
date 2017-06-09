d3d2b8b3866748c5918c7abff9f9deed5f6fe3b9
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-09 09:37:28 -0600

    Add/Implement NetworkController. Add TableViewController, and TableViewCell

---
+++ MovieSearch/MovieSearch/MovieTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Bradley GIlmore on 6/9/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    
+    
+}
